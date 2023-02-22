22f0a729c965daec7607a925bb371cb37e11b6f1
Jon Brighton <brightonsbox@hotmail.com>
2023-02-22 11:29:56 +0000

    Remove dodgy caseload code and fix h1 header

--- server/middleware/setUpCurrentUser.ts
+++ server/middleware/setUpCurrentUser.ts
@@ -1,13 +1,12 @@
 import { Router } from 'express'
 import auth from '../authentication/auth'
 import tokenVerifier from '../data/tokenVerification'
-import populateCurrentUser, { getUserActiveCaseLoad } from './populateCurrentUser'
+import populateCurrentUser from './populateCurrentUser'
 import type { Services } from '../services'
 
 export default function setUpCurrentUser({ userService }: Services): Router {
   const router = Router({ mergeParams: true })
   router.use(auth.authenticationMiddleware(tokenVerifier))
   router.use(populateCurrentUser(userService))
-  router.use(getUserActiveCaseLoad(userService))
   return router
 }
